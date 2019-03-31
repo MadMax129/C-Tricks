fa3da53d8d581a266473193f04ba9f9e68c7896a
MadMax129 <38307091+MadMax129@users.noreply.github.com>
2019-03-30 22:45:00 -0700

    Add files via upload

---
+++ wierd.c
@@ -0,0 +1,74 @@
+#if 0
+
+    /* Cool C tricks 
+    
+    Created by -> Maks S.
+
+    By the way this already is a fast way to comment out code
+    */
+    
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Dont want to use main define your own name
+#define MYNAME m##a##i##n
+
+int MYNAME()
+<% // Use these instead of {}
+
+int a = 1;
+int b = 10;
+int x = 10;
+
+// Secret Code 
+double m[]= {7709179928849219.0, 771};
+
+int arr <: 5 :>;
+arr<: 0 :> = 1;
+
+/* Ignore scanf input can be done with *:
+
+printf("Enter value for b\n");
+scanf("%*d", %b);
+
+*/
+
+switch (a)
+{
+    case 1:
+        if (b == 5)
+        {
+            // Change Case inside if statement
+            case 2:
+                printf("B is 5 and array is %d\n", arr<:0:>);
+                printf("Also Index can be used like %d\n", 0[arr]); // Or 0<:arr:>
+                printf("%d", printf("You can also print a word plus its length like this:"));
+
+            break;
+        }
+
+    // Change Case inside else statement
+    else case 3: 
+    {
+        printf("B not 5 and array is %d\n", arr<:0:>);
+        printf("Also Index can be used like %d\n", 0[arr]); // Or 0<:arr:>
+        printf(" %d is the length\n", printf("You can also print a word plus its length like this:"));
+        
+
+        case 4:
+            // Dont like C++ and want to print it out sercretly
+            for (int i = 0; i < 771; i++)
+            {
+                m[0] *= 2;
+            }
+            printf((char*) m);
+
+
+    }
+}
+
+return 0;
+
+%>
